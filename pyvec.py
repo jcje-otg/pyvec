29a2d7e336cd6f7b41e8c805d44966afcc6ba739
jcje <jcje@otg.dk>
2020-03-19 03:47:20 +0100

    Added subtraction

--- pyvec.py
+++ pyvec.py
@@ -1,9 +1,14 @@
 def add(a,b):
     return (a[0]+b[0], a[1]+b[1])
 
+def sub(a,b):
+    return (a[0]-b[0], a[1]-b[1])
+
 v1 = (1,2)
 v2 = (3,4)
 
 v3 = add(v1,v2)
+v4 = sub(v1,v2)
 
 print(v3)
+print(v4)
